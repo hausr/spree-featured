665d2148abade4b660a05d023c33cef09c7cca86
hausr <marcohaus@gmail.com>
2012-09-04 19:40:18 +0300

    Update db/migrate/20101221123800_add_product_featured_field.rb

--- db/migrate/20101221123800_add_product_featured_field.rb
+++ db/migrate/20101221123800_add_product_featured_field.rb
@@ -1,9 +1,9 @@
 class AddProductFeaturedField < ActiveRecord::Migration
-  def self.up
+  def up
     add_column :products, :featured, :boolean, :default => false, :null => false
   end
 
-  def self.down
+  def down
     remove_column :products, :featured
   end
 end
