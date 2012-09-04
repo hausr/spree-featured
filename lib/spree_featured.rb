4168f7628f3761461f9d827ed50965bb6d7f28fa
hausr <marcohaus@gmail.com>
2012-09-04 19:13:26 +0300

    Update lib/spree_featured.rb

--- lib/spree_featured.rb
+++ lib/spree_featured.rb
@@ -11,10 +11,28 @@ module SpreeFeatured
         Rails.env.production? ? require(c) : load(c)
       end
 
-      Product.class_eval do
+      Spree::Product.class_eval do
         scope :featured,:conditions => ['deleted_at is null and featured = ?', true]
       end
-
+      
+      Deface::Override.new(
+        :virtual_path  => "spree/admin/products/index",
+        :insert_after => '[data-hook="admin_product_form_right"]',
+        :partial          => 'admin/products/spree_featured_admin_product_fields.html',
+        :name => "product_fields"
+      )
+      Deface::Override.new(
+        :virtual_path  => "spree/admin/products/index",
+        :insert_after => '[data-hook="admin_products_index_rows"]',
+        :partial          => 'admin/products/spree_featured_admin_product_listing.html',
+        :name => "product_listing"
+      )
+      Deface::Override.new(
+        :virtual_path  => "spree/admin/products/index",
+        :insert_after => '[data-hook="admin_products_index_headers"]',
+        :partial          => 'admin/products/spree_featured_admin_product_headers.html',
+        :name => "product_header"
+      )
     end
 
     config.to_prepare &method(:activate).to_proc
